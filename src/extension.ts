45d9441faaf5114fbc3dd9e766f0a1f14da26e90
Girotto <l.girottoph@gmail.com>
2023-10-25 10:03:41 +0100

    refactored code

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,11 @@
 import * as vscode from "vscode";
 import * as Tree from "./Tree";
 
-function startTreeView(context: vscode.ExtensionContext) {
-  const treeDataProvider = new Tree.TimerDataProvider();
-  const treeView = vscode.window.createTreeView("speedrunTimer", {
-    treeDataProvider,
-  });
+// Constants
+const SAVE_SPEEDRUN = "Save";
+const DISCARD_SPEEDRUN = "Discard";
 
-  let speedrunLogs = context.globalState.get("speedrun-logs", "");
-  if (speedrunLogs) {
-    let timers = JSON.parse(speedrunLogs);
-    treeDataProvider.loadTimers(timers);
-  }
-  context.subscriptions.push(treeView);
 
-  return treeDataProvider;
-}
-
-function createStatusItem() {
-  const statusItem = vscode.window.createStatusBarItem(
-    vscode.StatusBarAlignment.Right
-  );
-  statusItem.text = `$(watch) Start speedrun`;
-  statusItem.command = "speedrun-timer.start-timer";
-  statusItem.tooltip = "Click to start the speedrun timer.";
-  statusItem.show();
-
-  return statusItem;
-}
 export function activate(context: vscode.ExtensionContext) {
   // Greet the user
   console.log(
@@ -45,13 +23,15 @@ export function activate(context: vscode.ExtensionContext) {
   // Register a command to delete a speedrun log
   vscode.commands.registerCommand("speedrun-timer.delete-timer", (node) => {
     treeDataProvider.removeTimer(node);
-    let timers = JSON.stringify(treeDataProvider.getTimers());
+    const timers = JSON.stringify(treeDataProvider.getTimers());
     context.globalState.update("speedrun-logs", timers);
     treeDataProvider.refresh();
   });
 
   // Create a status bar item so the user can activate timers
-  const statusItem = createStatusItem();
+  const startButton = createStatusBarStart();
+  let statusTimer: vscode.StatusBarItem;
+  let intervalTimer: NodeJS.Timeout;
 
   // Register a command to start a speedrun timer
   context.subscriptions.push(
@@ -59,83 +39,50 @@ export function activate(context: vscode.ExtensionContext) {
       // Set running to true. Used to disable the start timer
       vscode.commands.executeCommand("setContext", "isRunning", true);
 
-      statusItem.hide();
+      startButton.hide();
       // Add a timer to the status bar
-      let timer = vscode.window.createStatusBarItem(
-        vscode.StatusBarAlignment.Right
-      );
-      timer.text = "0hr 0m 0s";
-      timer.command = "speedrun-timer.stop-timer";
-      timer.tooltip = "Stop the speedrun timer";
-      timer.show();
+      statusTimer = createStatusBarTimer();
 
       // Start the timer
-      const intervalTimer = createTimer(timer);
+      intervalTimer = createTimer(statusTimer);
 
       // Show a message box to the user
       vscode.window.showInformationMessage(
         "Speedrun timer started. Get to coding!"
       );
+    })
+  );
 
-      // Stop the timer
-      let stopTimer = vscode.commands.registerCommand(
-        "speedrun-timer.stop-timer",
-        () => {
-          // Set context to false
-          vscode.commands.executeCommand("setContext", "isRunning", false);
-          let endTime = timer.text;
-          clearInterval(intervalTimer);
-          timer.hide();
-          statusItem.show();
+  // Register command to stop a speedrun timer
+  context.subscriptions.push(
+    vscode.commands.registerCommand("speedrun-timer.stop-timer", () => {
+      // Set running to false so the start timer can be used again.
+      vscode.commands.executeCommand("setContext", "isRunning", false);
 
-          vscode.window
-            .showInformationMessage(
-              `Nice work! Your time was: ${endTime}. Would you like to save your speedrun?`,
-              "Save",
-              "Discard"
-            )
-            .then((value) => {
-              // Save speedrun time
-              if (value === "Save") {
-                vscode.window
-                  .showInputBox({
-                    prompt: "Enter a name for your speedrun",
-                    placeHolder: "Project Name",
-                  })
-                  .then((value) => {
-                    if (value === undefined) {
-                      vscode.window.showInformationMessage(
-                        "Speedrun not saved."
-                      );
-                      return;
-                    }
+      // Store end time for further use
+      const endTime = statusTimer.text;
 
-                    // Save to the activity bar log
-                    treeDataProvider.addTimer(
-                      new Tree.TimerTreeItem(`${value}`, [
-                        new Tree.TimerTreeItem(
-                          `Date: ${new Date().toDateString()}`
-                        ),
-                        new Tree.TimerTreeItem(`Time: ${endTime}`),
-                      ])
-                    );
-                    treeDataProvider.refresh();
-                    vscode.window.showInformationMessage(
-                      "Speedrun saved to activity bar log."
-                    );
+      // Remove interval timer from stack
+      clearInterval(intervalTimer);
 
-                    // Update global state with a json string of the timers
-                    let timers = JSON.stringify(treeDataProvider.getTimers());
-                    context.globalState.update("speedrun-logs", timers);
-                  });
-              } else {
-                vscode.window.showInformationMessage("Speedrun not saved.");
-              }
-            });
+      // Hide status bar timer and show start button so the user can start another speedrun.
+      statusTimer.hide();
+      startButton.show();
 
-          stopTimer.dispose();
-        }
-      );
+      vscode.window
+        .showInformationMessage(
+          `Nice work! Your time was: ${endTime}. Would you like to save your speedrun?`,
+          SAVE_SPEEDRUN,
+          DISCARD_SPEEDRUN
+        )
+        .then((value) => {
+          // Save speedrun time
+          if (value === SAVE_SPEEDRUN) {
+            saveSpeedrun(context, treeDataProvider, endTime);
+          } else {
+            vscode.window.showInformationMessage("Speedrun not saved.");
+          }
+        });
     })
   );
 }
@@ -145,6 +92,80 @@ export function deactivate() {
   vscode.window.showInformationMessage(
     "Thank you for using Speedrun Timer! Come back soon!"
   );
+}
+
+function startTreeView(context: vscode.ExtensionContext) {
+  const treeDataProvider = new Tree.TimerDataProvider();
+  const treeView = vscode.window.createTreeView("speedrunTimer", {
+    treeDataProvider,
+  });
+
+  let speedrunLogs = context.globalState.get("speedrun-logs", "");
+  if (speedrunLogs) {
+    const timers = JSON.parse(speedrunLogs);
+    treeDataProvider.loadTimers(timers);
+  }
+  context.subscriptions.push(treeView);
+
+  return treeDataProvider;
+}
+
+function createStatusBarStart() {
+  const statusItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right
+  );
+  statusItem.text = `$(watch) Start speedrun`;
+  statusItem.command = "speedrun-timer.start-timer";
+  statusItem.tooltip = "Click to start the speedrun timer.";
+  statusItem.show();
+
+  return statusItem;
+}
+
+function createStatusBarTimer() {
+  const timer = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right
+  );
+  timer.text = "0hr 0m 0s";
+  timer.command = "speedrun-timer.stop-timer";
+  timer.tooltip = "Stop the speedrun timer";
+  timer.show();
+
+  return timer;
+}
+
+function saveSpeedrun(
+  context: vscode.ExtensionContext,
+  treeDataProvider: Tree.TimerDataProvider,
+  endTime: string
+) {
+  vscode.window
+    .showInputBox({
+      prompt: "Enter a name for your speedrun",
+      placeHolder: "Project Name",
+    })
+    .then((value) => {
+      if (value === undefined) {
+        vscode.window.showInformationMessage("Speedrun not saved.");
+        return;
+      }
+
+      // Save to the activity bar log
+      treeDataProvider.addTimer(
+        new Tree.TimerTreeItem(`${value}`, [
+          new Tree.TimerTreeItem(`Date: ${new Date().toDateString()}`),
+          new Tree.TimerTreeItem(`Time: ${endTime}`),
+        ])
+      );
+      treeDataProvider.refresh();
+      vscode.window.showInformationMessage(
+        "Speedrun saved to activity bar log."
+      );
+
+      // Update global state with a json string of the timers
+      let timers = JSON.stringify(treeDataProvider.getTimers());
+      context.globalState.update("speedrun-logs", timers);
+    });
 }
 
 function createTimer(timer: vscode.StatusBarItem) {
